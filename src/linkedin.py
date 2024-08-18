82286c024eccba7a1ac3959d4179c640dd00f546
kevthefoo <kevthefoo@gmail.com>
2024-08-19 00:27:34 +1000

    linkedin 91%

--- src/linkedin.py
+++ src/linkedin.py
@@ -1,5 +1,6 @@
-import time
-import config
+import time, config
+
+from src.message import sendMessage
 
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
--- src/message.py
+++ src/message.py
@@ -3,12 +3,10 @@ import requests, dotenv, os
 # Load the environment variables
 dotenv.load_dotenv()
 TOKEN = os.getenv('TOKEN')
-CHANNEL_1 = os.getenv('CHANNEL_1')
-
 
 # Set the header
 header = {'authorization': TOKEN}
 
-def sendMessage(content):
+def sendMessage(content, channel):
     payload = {"content": content, "tts": False, "flags": 0}
-    requests.post(CHANNEL_1, headers=header, data=payload)
+    requests.post(channel, headers=header, data=payload)
