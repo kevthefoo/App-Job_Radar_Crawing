577b2997d7b7f9a27c5e30a8a5160e9efd7eaec0
kevthefoo <kevthefoo@gmail.com>
2024-08-24 11:52:43 +1000

    normal modified

--- src/message.py
+++ src/message.py
@@ -3,10 +3,12 @@ import requests, dotenv, os
 # Load the environment variables
 dotenv.load_dotenv()
 TOKEN = os.getenv('TOKEN')
+CHANNEL_HOOK = os.getenv('CHANNEL_HOOK')
+HOOK_CHANNEL_URL = f"https://discord.com/api/v9/channels/{CHANNEL_HOOK}/messages"
 
 # Set the header
 header = {'authorization': TOKEN}
 
-def sendMessage(content, channel):
+def sendMessage(content):
     payload = {"content": content, "tts": False, "flags": 0}
-    requests.post(channel, headers=header, data=payload)
+    requests.post(HOOK_CHANNEL_URL, headers=header, data=payload)
--- z.py
+++ z.py
@@ -1,33 +1,7 @@
-# Problem:
-# There will be a party in my home on Sunday. I will divide the people into 4 groups. The first group will be the kids group (age < 18). The second group will be Group A (age % 3 == 0). The third group will be Group B (age % 3 == 1). The rest of the people will be in Group C. Each group will sit in a table where males will sit in the left side and females will sit in the right side.
+from src.message import sendMessage
 
 
-# Obtain input from the user
-age = int(input("Enter your age: "))
-sex = input("Enter your sex (male/female): ").lower()
 
-# Validate the age
-if age < 0:
-    print("Something went wrong. Please enter a valid age.")
-# Validate the sex
-elif sex is not "male" or sex is not "female":
-    print("Something went wrong. Please enter a valid sex.")
-else:
-    # Determine the message based on age and sex
-    if age < 18:
-        group = 'Kid Group'
-    elif age % 3 == 0:
-        group = "Group A"
-    elif age % 3 == 1:
-        group = "Group B"
-    else:
-        group = "Group C"
+content = ''
 
-    if sex == "male":
-        sitting_message = "You are a male. Please sit in the left section."
-    else:
-        sitting_message = "You are a female. Please sit in the right section."
-
-    # Output the results
-    print(f"You are in {age}.")
-    print(sitting_message)
+sendMessage('Job Title:\nTechnical Game Designer - League of Legends, Game Modes\n\nLocation:\nQueensland\n\nCompany:\nGoogle\n\nJob URL:\nhttps://google.com\n\nSource: \nLinkedIn')
